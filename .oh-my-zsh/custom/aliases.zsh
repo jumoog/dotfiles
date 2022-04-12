18641bfb6e949f920864f8db98cbf85b236f3757
Kilian von Pflugk <github@jumoog.io>
2022-04-12 11:23:06 +0000

    add megacmd container

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,1 +1,2 @@
 alias http="http-server -S -C /etc/letsencrypt/live/s1.jumoog.io/cert.pem -K /etc/letsencrypt/live/s1.jumoog.io/privkey.pem"
+alias megaput="docker run --rm -ti -v ${HOME}/.megaCmd:/root/.megaCmd -v /home:/up ghcr.io/jumoog/megacmd:latest mega-put ${1} ${2}"
